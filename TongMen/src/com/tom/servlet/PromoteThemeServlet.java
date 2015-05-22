83f5738032f04a95a73f55023681b5a5f50e877d
suxiaomo <415564898@qq.com>
2015-05-22 20:36:11 +0800

    15/05/22 完善内容 sxm

---
+++ TongMen/src/com/tom/servlet/PromoteThemeServlet.java
@@ -0,0 +1,47 @@
+package com.tom.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+
+import com.tom.Service.PromoteService;
+
+public class PromoteThemeServlet {
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PromoteThemeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		JSONArray array = new JSONArray();
+		
+		PromoteService promoteservice = new PromoteService();
+		
+		array =promoteservice.PromoteTheme();
+		response.getWriter().print(array);
+			
+	}
+}
